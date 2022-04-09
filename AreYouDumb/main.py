f63cd7a1e121dd3e2d18306de5e2dedc058a2d80
yungcypo <80849671+yungcypo@users.noreply.github.com>
2022-04-09 13:06:11 +0200

    Add files via upload

--- AreYouDumb/main.py
+++ AreYouDumb/main.py
@@ -13,6 +13,7 @@ colors = {
     "white" : (255, 255, 255),
     "gray" : (20, 20, 20),
     "lightgray" : (50, 50, 50),
+    "lightergray" : (100, 100, 100),
     "black" : (0, 0, 0)}
 
 
@@ -27,16 +28,24 @@ text_rect = text_surf.get_rect(midbottom = (W/2, H/3))
 finaltext_surf = font.render("I knew it!", True, colors["white"])
 finaltext_rect = finaltext_surf.get_rect(center = (W/2, H/2))
 
+def reset():
+    global dumb
+    dumb = False
+    button_no.center = (W/4*3, H/3*2)
+    button_yes.center = (W/4, H/3*2)
 
 class Button:
     def __init__(self, center, text):
         self.center = center
         self.text = text
-        self.size = int(W/8)
+        self.size = int(W/7)
         self.surf = pygame.surface.Surface((self.size, self.size))
         self.rect = self.surf.get_rect(center = self.center)
         self.text_surf = font_small.render(self.text, True, colors["white"])
         self.color = colors["gray"]
+        self.hover = False
+        self.clicked = False
+
 
     def draw(self):
         self.rect = self.surf.get_rect(center = self.center)
@@ -51,19 +60,22 @@ class Button:
         (self.rect.topright, self.rect.topleft, self.rect.bottomleft, self.rect.bottomright)
         )
         
-        if self.rect.collidepoint(pygame.mouse.get_pos()) and self.text == "Yes":
+        if self.hover:
             self.color = colors["lightgray"]
+            if self.clicked:
+                self.color = colors["lightergray"]
         else:
             self.color = colors["gray"]
 
         SCREEN.blit(self.text_surf, self.text_rect)
 
+
     def reposition(self):
         self.center = (random.choice(range(int(W/2), int(W - self.size/2))), 
             random.choice(range(int(H/2), int(H - self.size/2))))
 
-button_yes = Button((W/4, H/2), "Yes")
-button_no = Button((W/4*3, H/2), "No")
+button_yes = Button((W/4, H/3*2), "Yes")
+button_no = Button((W/4*3, H/3*2), "No")
 
 
 def main():
@@ -75,24 +87,38 @@ def main():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and dumb:
-                    dumb = False
-                    button_no.center = (W/4*3, H/2)
-            if button_no.rect.collidepoint(MOUSE):
-                button_no.reposition()
-            if pygame.mouse.get_pressed()[0]:
+                        reset()
+
+            MOUSE = pygame.mouse.get_pos()
+            if button_yes.rect.collidepoint(MOUSE):
+                button_yes.hover = True
+                if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+                    button_yes.clicked = True
+            else:
+                button_yes.hover = False
+            
+            if event.type == pygame.MOUSEBUTTONUP:
+                button_yes.clicked = False
+                if dumb:
+                    reset()
                 if button_yes.rect.collidepoint(MOUSE):
                     dumb = True
+                    
+            if button_no.rect.collidepoint(MOUSE):
+                button_no.reposition()
 
-        MOUSE = pygame.mouse.get_pos()
 
         SCREEN.fill(colors["gray"])
         SCREEN.blit(text_surf, text_rect)
         button_yes.draw()
         button_no.draw() 
 
+
         if dumb:
+            button_yes.rect.bottomright = (0,0)
             SCREEN.fill(colors["gray"])
             SCREEN.blit(finaltext_surf, finaltext_rect)
+            
 
         CLOCK.tick(FPS)
         pygame.display.update()
